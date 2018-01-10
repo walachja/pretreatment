986e78149e8766f339569f22d8f8ad42228374b4
Jan Walach <jan.walach@tuwien.ac.at>
2018-01-10 17:17:56 +0100

    Add files via upload

---
+++ R/LinearBaseline.R
@@ -0,0 +1,31 @@
+#' LinearBaseline
+#'
+#'
+#'
+#' @name LinearBaseline
+#' @author Jan Walach
+#' @param x data matrix with rows as samples and columns as variables
+#' @param prd.td cutoff parameter
+
+#' @description Linear Baseline normalization.
+#' @author Jan Walach
+#' @details Linear Baseline normalization as described in [1].
+#' @return The function returns object of type "matrix".
+#' \item{x}{  matrix after normalization}
+
+#' @export
+#' @references [1] ADD NAME OF BOOK AND CHAPTER
+#' @examples
+#' data(mcad)
+#' x <- mcad[,-1]
+#' LinearBaseline(x)
+
+LinearBaseline <- function(x)
+{
+  x <- t(x)
+  linear.baseline <- apply(x,1,median)
+  baseline.mean <- mean(linear.baseline)
+  sample.means <- apply(x,2,mean)
+  linear.scaling <- baseline.mean/sample.means
+  return( t(t(t(x)*linear.scaling)) )
+}
---
+++ R/NonLinearBaseline.R
@@ -0,0 +1,46 @@
+#' NonLinearBaseline
+#'
+#'
+#'
+#' @name NonLinearBaseline
+#' @author Jan Walach
+#' @param x data matrix with rows as samples and columns as variables
+#' @param prd.td cutoff parameter
+
+#' @description Non-Linear Baseline normalization.
+#' @author Jan Walach
+#' @details Non-Linear Baseline normalization as described in [1]. The function is based on function from Affy package [2]
+#' @return The function returns object of type "matrix".
+#' \item{liwong.x}{  matrix after normalization}
+
+#' @export
+#' @references [1] ADD NAME OF BOOK AND CHAPTER
+#' @references [2] L.Gautier and L. Cope and B.M. Bolstad and R. A. Irizarry (2004) affy---analysis of Affymetrix GeneChip data at the probe level, Bioinformatics, 20(3), 307--315, 2004
+#' @examples
+#' data(mcad)
+#' x <- mcad[,-1]
+#' NonLinearBaseline(x,prd.td=c(0.003,0.007))
+
+NonLinearBaseline <- function(x,prd.td=c(0.003,0.007))
+{
+  x <- t(x)
+  library(affy)
+
+  # First step: Find baseline sample
+  average.intensity <- apply(x,2,mean)
+  median.number <- round(ncol(x)/2 + 0.1) # additional 0.1 ensures that it rounds properly
+  ordering <- order(average.intensity)
+  median.sample.number <- ordering[median.number]
+  median.sample <- x[,median.sample.number]
+
+  # Apply normalization
+  liwong.x=vector()
+  for(i in 1:ncol(x)){
+    liwong.model <- normalize.invariantset(data=x[,i],
+                                           ref=median.sample,
+                                           prd.td=prd.td)
+    liwong.sample <- predict(liwong.model$n.curve$fit,x[,i])
+    liwong.x <- cbind(liwong.x,liwong.sample$y)
+  }
+  return(t(liwong.x))
+}
